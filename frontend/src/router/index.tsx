0673dc6310858ac52204a3203db20a5ff7dbe3c0
Andres Gallo <felipegallom1011@gmail.com>
2024-05-31 20:14:37 -0500

    Error handling

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -5,6 +5,7 @@ import Characters from '../views/characters/Index'
 import { useUser } from '../hooks/useUser'
 import { Route, Routes } from 'react-router-dom'
 import ProtectedRoute from '../components/protectedRoute'
+import FavoriteList from '../components/favorites/FavoriteList'
 
 export const RoutesList = () => {
   const { isLogged } = useUser()
@@ -15,7 +16,7 @@ export const RoutesList = () => {
       <Route
         element={<ProtectedRoute canAccess={isLogged} redirect='/login' />}
       >
-        <Route path='/favorites' element={<h1>Favorites</h1>} />
+        <Route path='/favorites' element={<FavoriteList />} />
       </Route>
       <Route path='/login' element={<Login />} />
       <Route path='/register' element={<Register />} />
