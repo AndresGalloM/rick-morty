9466169d890e74716189e0ea19a16a2f77bd5c54
Andres Gallo <felipegallom1011@gmail.com>
2024-06-07 21:25:49 -0500

    Hash router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import '@fontsource-variable/onest'
 import './index.css'
 import { ThemeProvider } from './context/theme.tsx'
@@ -9,11 +9,11 @@ import { ThemeProvider } from './context/theme.tsx'
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <HashRouter>
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
         <App />
       </ThemeProvider>
     </QueryClientProvider>
-  </BrowserRouter>
+  </HashRouter>
 )
